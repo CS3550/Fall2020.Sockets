7b6f5f04482086f9e22fadb861324e3e7b17aca8
Ricks <bricks@unomaha.edu>
2020-08-31 04:03:07 -0500

    Create ThreadedSocket.java

---
+++ ThreadedSocket.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class ThreadedSocket implements Runnable {
+
+    private final Socket clientSocket;
+
+    public ThreadedSocket(Socket clientSocket) {
+
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+
+        PrintStream out = null;
+        try {
+            out = new PrintStream(clientSocket.getOutputStream());
+            out.println("HTTP/1.0 200 OK\n\nHello, internet");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            out.close();
+        }
+    }
+}
